a60ba794cb3690d382ab30a31ac010bdebe932ea
Balaraj R <balarajr483@gmail.com>
2025-07-11 04:23:12 +0000

    var admin = require("firebase-admin");
    var serviceAccount = require("pa

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,23 +1,11 @@
 'use server';
 
 import * as admin from 'firebase-admin';
-
-const projectId = process.env.FIREBASE_PROJECT_ID;
-const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-// The private key needs to have its escaped newlines replaced with actual newlines
-const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
-
-if (!projectId || !clientEmail || !privateKey) {
-    throw new Error('Firebase Admin SDK environment variables are not set.');
-}
+import serviceAccount from '../../../serviceAccountKey.json';
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert({
-            projectId,
-            clientEmail,
-            privateKey,
-        }),
+        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     });
 }
 
