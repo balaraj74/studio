abc28fa3744698b1d9bbf9325becb926d0642bba
Balaraj R <balarajr483@gmail.com>
2025-07-11 08:22:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,14 +1,14 @@
-'use server';
-
 import { initializeApp, getApps, cert, type ServiceAccount } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import serviceAccount from '../../../serviceAccountKey.json';
 
 let adminDb: FirebaseFirestore.Firestore;
 
+const serviceAccountConfig = serviceAccount as ServiceAccount;
+
 if (!getApps().length) {
     initializeApp({
-        credential: cert(serviceAccount as ServiceAccount),
+        credential: cert(serviceAccountConfig),
     });
 }
 adminDb = getFirestore();
