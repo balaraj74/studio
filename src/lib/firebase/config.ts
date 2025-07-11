b860711ad8f0a433a768a791c2489da492588dbe
Balaraj R <balarajr483@gmail.com>
2025-07-11 07:24:47 +0000

    Error: Google Maps JavaScript API error: ApiTargetBlockedMapError https:

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,11 +1,12 @@
 
+
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getFirestore, type Firestore } from "firebase/firestore";
 import { getStorage, type FirebaseStorage } from "firebase/storage";
 import { getAuth, type Auth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY, // Using Maps key to resolve permission issue
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
@@ -21,3 +22,4 @@ const db: Firestore = getFirestore(app);
 const storage: FirebaseStorage = getStorage(app);
 
 export { app, db, storage, auth };
+
