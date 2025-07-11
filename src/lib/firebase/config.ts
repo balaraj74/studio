ed18065d789ceb5178be007c3a33e52d13a819c3
Balaraj R <balarajr483@gmail.com>
2025-07-11 14:19:36 +0000

    still the same error northing is fixed

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,5 +1,4 @@
 
-
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getFirestore, type Firestore } from "firebase/firestore";
 import { getStorage, type FirebaseStorage } from "firebase/storage";
@@ -16,10 +15,15 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app: FirebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
+let app: FirebaseApp;
+if (!getApps().length) {
+    app = initializeApp(firebaseConfig);
+} else {
+    app = getApp();
+}
+
 const auth: Auth = getAuth(app);
 const db: Firestore = getFirestore(app);
 const storage: FirebaseStorage = getStorage(app);
 
 export { app, db, storage, auth };
-
