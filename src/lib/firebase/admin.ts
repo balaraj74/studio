1fa1f51e44b4acdd978dd9c632e0feef12138c68
Balaraj R <balarajr483@gmail.com>
2025-07-12 09:03:06 +0000

    i made these changes

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,17 +1,22 @@
-import { initializeApp, getApps, cert, type ServiceAccount } from 'firebase-admin/app';
+
+import { initializeApp, getApps, cert, type App, type ServiceAccount } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import serviceAccount from '../../../serviceAccountKey.json';
 
+let adminApp: App;
 let adminDb: FirebaseFirestore.Firestore;
 
 const serviceAccountConfig = serviceAccount as ServiceAccount;
 
 if (!getApps().length) {
-    initializeApp({
+    adminApp = initializeApp({
         credential: cert(serviceAccountConfig),
     });
+} else {
+    adminApp = getApps()[0];
 }
-adminDb = getFirestore();
+
+adminDb = getFirestore(adminApp);
 
 /**
  * Returns a server-side Firestore instance.
