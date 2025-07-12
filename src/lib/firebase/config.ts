01bc22593ea9f71a96b7076e0e77f02cd33d1eac
Balaraj R <balarajr483@gmail.com>
2025-07-12 08:38:38 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -6,13 +6,12 @@ import { getAuth, type Auth } from "firebase/auth";
 
 // Your web app's Firebase configuration, as provided.
 const firebaseConfig = {
-  apiKey: "AIzaSyAd8T2SnKYd0lC464LCU8SPloORnCtf2f8",
-  authDomain: "agrisence-1dc30.firebaseapp.com",
-  projectId: "agrisence-1dc30",
-  storageBucket: "agrisence-1dc30.appspot.com",
-  messagingSenderId: "948776556057",
-  appId: "1:948776556057:web:59c34ba4ceffdd5901bc88",
-  measurementId: "G-NZ199RVD5G"
+  apiKey: "AIzaSyCnd63dfnoOxV-DxclWmmSEQjSC049L6bY",
+  authDomain: "agrisence.firebaseapp.com",
+  projectId: "agrisence",
+  storageBucket: "agrisence.firebasestorage.app",
+  messagingSenderId: "878440506403",
+  appId: "1:878440506403:web:c704f2284762b6332abd96"
 };
 
 
