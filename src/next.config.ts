d00e4d0ada36a3a49fceca9bc04f05a89866e64c
Balaraj R <balarajr483@gmail.com>
2025-09-06 10:01:46 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ src/next.config.ts
@@ -0,0 +1,29 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    // This is required to allow the Next.js dev server to accept requests from the preview iframe.
+    allowedDevOrigins: ["*.cloudworkstations.dev"],
+  },
+};
+
+export default nextConfig;
