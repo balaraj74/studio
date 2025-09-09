abc5f0de958ac2bf35dc68964c4ddccbe47c4445
Balaraj R <balarajr483@gmail.com>
2025-09-09 17:15:38 +0000

    Try fixing this error: `Console Error: FirebaseError: Messaging: A probl

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,4 @@
 
-// This service worker is intentionally left blank. 
-// Firebase will automatically handle background notifications.
+// This service worker file is intentionally left blank.
+// The Firebase SDK will dynamically handle the service worker logic.
+// See: https://firebase.google.com/docs/cloud-messaging/js/client#retrieve-the-current-registration-token
