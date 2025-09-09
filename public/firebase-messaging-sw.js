b6668427fafd0d4ce5b2a5ec914431386a4c9f67
Balaraj R <balarajr483@gmail.com>
2025-09-09 17:10:15 +0000

    Try fixing this error: `Console Error: FirebaseError: Messaging: A probl

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,34 +1,3 @@
 
-// Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
-
-// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
-const firebaseConfig = {
-  apiKey: "AIzaSyAd8T2SnKYd0lC464LCU8SPloORnCtf2f8",
-  authDomain: "agrisence-1dc30.firebaseapp.com",
-  projectId: "agrisence-1dc30",
-  storageBucket: "agrisence-1dc30.firebasestorage.app",
-  messagingSenderId: "948776556057",
-  appId: "1:948776556057:web:59c34ba4ceffdd5901bc88",
-  measurementId: "G-NZ199RVD5G"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-
-// Retrieve an instance of Firebase Messaging so that it can handle background messages.
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/agrisence-logo-192.png' // Ensure you have this icon in your public folder
-  };
-
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
+// This service worker is intentionally left blank. 
+// Firebase will automatically handle background notifications.
