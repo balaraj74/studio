fb297577dd3afeb588e7540b0dd5fff73ea63ea1
Balaraj R <balarajr483@gmail.com>
2025-09-10 18:45:31 +0000

    QqXsqHjpCn-22R2id_sYpiW7fOemxFSZTw5fMlEh3Zs

--- next.config.ts
+++ next.config.ts
@@ -28,12 +28,9 @@ const nextConfig: NextConfig = {
     // This is required to allow the Next.js dev server to accept requests from the preview iframe.
     // In newer Next.js versions, this might be handled automatically or via a different config.
   },
-  watchOptions: {
-    ignored: ['**/src/ai/.genkit/**'],
-  },
   env: {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-    NEXT_PUBLIC_FIREBASE_VAPID_KEY: 'BKy-UaL9-3-sW_Gz5G_wDso9-y_SbYxAqncf27lJ3D-u-Y9j-tA6-i_lR-oGzM-pX_r-A6sB8cZ_eL-4KjY-jJk',
+    NEXT_PUBLIC_FIREBASE_VAPID_KEY: 'QqXsqHjpCn-22R2id_sYpiW7fOemxFSZTw5fMlEh3Zs',
   },
 };
 
